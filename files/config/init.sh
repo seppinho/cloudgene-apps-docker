da89fa85f205c3c10e29efcc8120eaf1e726a0dd
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2016-08-18 12:43:05 +0200

    remove pandoc

--- files/config/init.sh
+++ files/config/init.sh
@@ -1,3 +1,2 @@
 
 sudo cp /opt/cloudgene/applications/cloudgene-apps-docker/files/config/settings.yaml /opt/cloudgene/config/settings.yaml
-sudo ln -s /opt/cloudgene/applications/cloudgene-apps-docker/files/pandoc /usr/local/bin
